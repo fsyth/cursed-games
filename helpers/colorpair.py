4f574a362346bf1d955faac0e579a30a5dd55be8
James Forsyth <forsythjames@hotmail.co.uk>
2026-08-20 20:23:14 +0100

    Added details to ColorPair docstring

--- helpers/colorpair.py
+++ helpers/colorpair.py
@@ -1,7 +1,17 @@
 import curses
 
 class ColorPair:
-    """Curses color pair, with id, foreground color, and background color, for xterm-256color."""
+    """
+    Curses color pair, with id, foreground color,
+    and background color, for xterm-256color.
+    | Range   | Colors                        |
+    | :-----: | :---------------------------- |
+    |     -1  | Terminal default              |
+    |   0-15  | Standard terminal colours x16 |
+    |  16-231 | RGB cube 6x6x6                |
+    | 232-255 | Greyscale shades x24          |
+    """
+
     counter = 0
 
     def __init__(self, fg: int, bg: int):
